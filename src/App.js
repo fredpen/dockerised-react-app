557ccfd88d374ceabc4b9e79feb75506c1c8023a
Fredrick Oladipupo <fredrick.oladipupo@gimo.co.uk>
2024-06-09 12:12:54 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          I know react, but i need more than that
         </a>
       </header>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,13 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/i know react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
+test('renders learn reacct link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/i know react/i);
   expect(linkElement).toBeInTheDocument();
 });
